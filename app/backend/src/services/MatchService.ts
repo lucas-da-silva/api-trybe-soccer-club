462894881f2d0cfd35cdf8211b881cd5832e3d24
lucasdasilva <xBigodinho@outlook.com>
2023-01-23 15:54:19 -0300

    feat: create function getInProgress in MatchService

--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -6,8 +6,19 @@ class MatchService {
   getAll = async () => {
     const matches = await MatchModel.findAll({
       include: [
-        { model: TeamModel, as: 'homeTeam' },
-        { model: TeamModel, as: 'awayTeam' },
+        { model: TeamModel, as: 'homeTeam', attributes: ['teamName'] },
+        { model: TeamModel, as: 'awayTeam', attributes: ['teamName'] },
+      ],
+    });
+    return matches;
+  };
+
+  getInProgress = async (inProgress: boolean) => {
+    const matches = await MatchModel.findAll({
+      where: { inProgress },
+      include: [
+        { model: TeamModel, as: 'homeTeam', attributes: ['teamName'] },
+        { model: TeamModel, as: 'awayTeam', attributes: ['teamName'] },
       ],
     });
     return matches;
