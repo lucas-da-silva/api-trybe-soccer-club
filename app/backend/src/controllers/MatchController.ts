9b8a5e8627ca6c77e9ae760ec3d96cbed83a643a
lucasdasilva <xBigodinho@outlook.com>
2023-01-23 16:29:55 -0300

    refactor: implements conditional for getAll function to validate query strings

--- app/backend/src/controllers/MatchController.ts
+++ app/backend/src/controllers/MatchController.ts
@@ -4,8 +4,9 @@ import { MatchService } from '../services';
 class MatchController {
   constructor(private matchService: MatchService) {}
 
-  public getAll = async (_req: Request, res: Response) => {
-    const matches = await this.matchService.getAll();
+  public getAll = async (req: Request, res: Response) => {
+    const { inProgress } = req.query;
+    const matches = await this.matchService.getAll(inProgress as string | undefined);
     res.status(200).json(matches);
   };
 }
--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -3,24 +3,22 @@ import MatchModel from '../database/models/MatchModel';
 import TeamModel from '../database/models/TeamModel';
 
 class MatchService {
-  getAll = async () => {
-    const matches = await MatchModel.findAll({
-      include: [
-        { model: TeamModel, as: 'homeTeam', attributes: ['teamName'] },
-        { model: TeamModel, as: 'awayTeam', attributes: ['teamName'] },
-      ],
-    });
-    return matches;
-  };
+  getAll = async (inProgress: string | undefined) => {
+    let matches;
+    const includeTeamName = [
+      { model: TeamModel, as: 'homeTeam', attributes: ['teamName'] },
+      { model: TeamModel, as: 'awayTeam', attributes: ['teamName'] },
+    ];
 
-  getInProgress = async (inProgress: boolean) => {
-    const matches = await MatchModel.findAll({
-      where: { inProgress },
-      include: [
-        { model: TeamModel, as: 'homeTeam', attributes: ['teamName'] },
-        { model: TeamModel, as: 'awayTeam', attributes: ['teamName'] },
-      ],
-    });
+    if (inProgress !== undefined) {
+      matches = await MatchModel.findAll({
+        where: { inProgress: inProgress === 'true' },
+        include: includeTeamName,
+      });
+    } else {
+      matches = await MatchModel.findAll({ include: includeTeamName });
+    }
+
     return matches;
   };
 }
